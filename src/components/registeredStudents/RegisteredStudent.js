10f035036a2c30f1ebe7b46593196e918a6e82d3
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-11-02 18:55:13 +0200

    RegisteredStudent

---
+++ src/components/registeredStudents/RegisteredStudent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./RegisteredStudent.css"
+
+function RegisteredStudent(){
+    return(
+        <div>
+            <div className="registration-student__description">
+                <h2>Rossita</h2>
+                <div className="registration-student__course">
+                    Fullstack
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RegisteredStudent;
